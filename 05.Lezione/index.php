ed2bf4fa68e310ace4a8232d6be1d9ba3193bc2b
giorgionetg <giorgionetg@gmail.com>
2015-08-07 13:48:37 +0200

    edited gitignore to exclude composer.lock and some changes on 05.Lez

--- 05.Lezione/index.php
+++ 05.Lezione/index.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 
 $app->get('/', function () {
@@ -10,6 +13,20 @@ $app->get('/', function () {
 
 $app->get('/hello/{name}', function ($name) {
 	return "Hello " . ucfirst($name) . "!";
+});
+
+// Esempio di Post
+
+$app->match('/form', function(Request $request) {
+
+	$own_response = '<form method="POST">Saluta:<br /><input type="text" name="nome_saluto" /><br /><input type="submit" value="Saluta!"></form>';
+	
+	if ($request->get('nome_saluto')) {
+		$own_response = '<h1>Hello ' . $request->get('nome_saluto') . '</h1>';
+	}
+	
+	return new Response($own_response, 201);
+	return ;
 });
 
 
